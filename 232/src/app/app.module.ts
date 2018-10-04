abf5a3c72391004fbaa6a7f783bb40fb74d13bf6
angerson1722 <angersonlasso@yahoo.com.mx>
2018-10-03 20:33:47 -0500

    Revert "Error"
    This reverts commit deb549f38b0a632f44e2aa76911121f52d0940d2.

--- 232/src/app/app.module.ts
+++ 232/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { AdminPage } from '../pages/admin/admin';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -18,7 +17,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    AdminPage,
     TabsPage
   ],
   imports: [
@@ -31,7 +29,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    AdminPage,
     TabsPage
   ],
   providers: [
