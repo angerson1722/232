deb549f38b0a632f44e2aa76911121f52d0940d2
angerson1722 <angersonlasso@yahoo.com.mx>
2018-10-03 20:32:59 -0500

    Error

---
+++ 232/src/pages/admin/admin.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-admin',
+  templateUrl: 'home.admin'
+})
+export class AdminPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
