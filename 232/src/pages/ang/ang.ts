714a2633751c2382bf34a9857af2a64d37e2b792
angerson1722 <angersonlasso@yahoo.com.mx>
2018-10-05 21:02:14 -0500

    Error2Page

---
+++ 232/src/pages/ang/ang.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-ang',
+  templateUrl: 'ang.html'
+})
+export class AngPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
