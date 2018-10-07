2c9179f9e5cf62b9c7b9d46f2715dc00b01c6b63
angerson1722 <angersonlasso@yahoo.com.mx>
2018-10-06 21:06:41 -0500

    Error al crear logo deshacer

---
+++ 232/src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { AngComponent } from './ang/ang';
+@NgModule({
+	declarations: [AngComponent],
+	imports: [],
+	exports: [AngComponent]
+})
+export class ComponentsModule {}
