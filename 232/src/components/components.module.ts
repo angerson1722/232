eeb7a4d24081734d7090c18919f2f08270931a38
angerson1722 <angersonlasso@yahoo.com.mx>
2018-10-06 21:07:32 -0500

    Revert "Error al crear logo deshacer"
    This reverts commit 2c9179f9e5cf62b9c7b9d46f2715dc00b01c6b63.

--- 232/src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AngComponent } from './ang/ang';
-@NgModule({
-	declarations: [AngComponent],
-	imports: [],
-	exports: [AngComponent]
-})
-export class ComponentsModule {}
